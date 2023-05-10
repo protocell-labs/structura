9d2b336e034aefb97d81d0999d858164e910b099
Galo Canizares <galocanizares@Galos-MacBook-Pro.local>
2023-05-09 20:12:53 -0400

    added grain and modified rock shader

--- main.js
+++ main.js
@@ -183,7 +183,7 @@ function View(viewArea) {
   composer.setSize(window.innerWidth, window.innerHeight)
 
   // change scene background to solid color
-  scene.background = new THREE.Color('#080808'); //0xffffff, 0x000000
+  scene.background = new THREE.Color('#292929'); //0xffffff, 0x000000
 
   const color = 0xffffff; //0xffffff
   const amb_intensity = 0.1; //0-1, zero works great for shadows with strong contrast
@@ -699,6 +699,33 @@ View.prototype.addRock = function () {
   varying vec3 vPos;
   varying vec3 vNormal;
 
+  //simple noise
+  float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+  vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+  vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
+  
+  float noise(vec3 p){
+      vec3 a = floor(p);
+      vec3 d = p - a;
+      d = d * d * (3.0 - 2.0 * d);
+  
+      vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
+      vec4 k1 = perm(b.xyxy);
+      vec4 k2 = perm(k1.xyxy + b.zzww);
+  
+      vec4 c = k2 + a.zzzz;
+      vec4 k3 = perm(c);
+      vec4 k4 = perm(c + 1.0);
+  
+      vec4 o1 = fract(k3 * (1.0 / 41.0));
+      vec4 o2 = fract(k4 * (1.0 / 41.0));
+  
+      vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
+      vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+  
+      return o4.y * d.y + o4.x * (1.0 - d.y);
+  }
+
   float circle(in vec2 _st, in float _radius){
     vec2 dist = _st-vec2(0.5);
     return 1.-smoothstep(_radius-(_radius*0.01),
@@ -710,7 +737,7 @@ View.prototype.addRock = function () {
       vec3 norm = normalize(vNormal);
       float nDotL = clamp(dot(lightDirection, norm), 0.0, 0.2);
       float strength = step(nDotL, max(abs(vUv.x - 0.5),abs(vUv.y - 0.5)));
-      gl_FragColor = vec4(vPos+vec3(circle(vUv, strength*0.05)), 1.0);
+      gl_FragColor = vec4(vec3(0.8, 0.5, 0.2)*vec3(noise(vPos*2.0))+vec3(circle(vUv, strength*0.05)), 1.0);
   }`
 
   const outlineMat = new THREE.ShaderMaterial({
